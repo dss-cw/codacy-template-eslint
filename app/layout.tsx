1ccac541b922ab66fe1df7cfe717660563e28347
Dijalma Silva <dijalma@cloudwalk.io>
2023-07-03 18:07:47 -0300

    feat: config eslint more prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import {ReactNode} from "react";
-import {Metadata} from "next";
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
 
-import '../styles/global.scss'
+import '../styles/global.scss';
 
 export const metadata: Metadata = {
-    title: "Home Template",
-    description: "This is a template project",
-}
+  title: 'Home Template',
+  description: 'This is a template project',
+};
 
 export default function AppLayout({ children }: { children: ReactNode }) {
-    return (
-        <div>
-            <h1>App Layout</h1>
-            <br />
-            {children}
-        </div>
-    )
+  return (
+    <div>
+      <h1>App Layout</h1>
+      <br />
+      {children}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function App() {
-    return (
-        <div>
-            <h1>This is a Template Project</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is a Template Project</h1>
+    </div>
+  );
 }
